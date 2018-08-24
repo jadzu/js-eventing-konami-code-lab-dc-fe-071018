2fc6f59771f31c36d7e25606fde1846ed5f9c564
Andrew Zubiri <zubiri@gmail.com>
2018-08-24 00:28:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,13 +6,13 @@ let index = 0;
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === alphabet[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
@@ -21,5 +21,8 @@ function onKeyDownHandler(e) {
 
 function init() {
   // Write your JavaScript code inside the init() function
+
+let bodyEl = document.getElementByTagName("body");
+
 
 }
